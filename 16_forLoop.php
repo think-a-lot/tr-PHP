91563861544b2e83d13ca473e2ece2efa8380860
think_a_lot <39505587+think-a-lot@users.noreply.github.com>
2018-12-12 14:38:19 +0800

    004 - add 15,16,17,18

---
+++ 16_forLoop.php
@@ -0,0 +1,28 @@
+<?php
+  //echo "1, 2, 3, 4, 5, 6, 7, 8, 9, 10"; 
+  //output number 1-10 
+  for( $num = 1 ; $num <= 10 ; $num = $num + 1 ){
+    echo $num." ";
+  }
+
+  echo "<br><br>";
+
+  for( $num = 10; $num >= 1 ; $num = $num - 1 ){
+    echo $num." ";
+  }
+
+  echo "<br><br>";
+
+  for( $num = 1; $num <= 10 ; $num++ ){
+    echo $num." ";
+  }
+
+  echo "<br><br>";
+
+  for( $num = 10; $num >= 1 ; $num-- ){
+    echo $num." ";
+  }
+
+
+?>
+
---
+++ 18_forLoop_and_if.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <form action="" method="post">
+    Please enter a number
+    <input type="text" name="num_name">
+    <input type="submit" name="display_name" value="Display">
+  </form>
+  <br>
+</body>
+</html>
+
+<?php
+
+  if(isset($_POST['display_name'])){ 
+    $num = $_POST['num_name']; 
+    if( $num < 0 ){
+      echo "Negative input. Invalid";
+    }else{
+      for( $a = 1 ; $a <= $num ; $a++ ){ 
+        echo $a . " "; 
+      }
+    }
+  }
+
+?>
